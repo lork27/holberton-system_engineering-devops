b7d8114ab0fa3ff71c4884bb81408315990b6fd0
lork27 <g.lorca.lamadrid@gmail.com>
2022-01-03 13:11:19 -0400

    added task 2 and comments for all tasks

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,3 +1,4 @@
+# creates file
 file { '/tmp/school' :
   ensure  => file,
   owner   => 'www-data',
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+# executes a pkill command
+exec { 'killmenow':
+command => 'usr/bin/pkill killmenow'
+provider => 'shell',
+returns => [0, 1],
+}
