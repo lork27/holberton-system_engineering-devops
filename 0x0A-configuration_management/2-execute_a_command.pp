412a0642975fc42d742d3789d57fb4f160de4b4c
lork27 <g.lorca.lamadrid@gmail.com>
2022-01-03 16:00:05 -0400

    fixed lint issues

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,5 @@
 # executes a pkill command
 exec { 'killmenow':
-command  => 'usr/bin/pkill killmenow',
-provider => 'shell',
-returns  => [0, 1]
+command  => 'pkill killmenow',
+path     => 'usr/bin',
 }
