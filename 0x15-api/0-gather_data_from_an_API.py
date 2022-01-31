9c5134a584f198a10efecfa91b6cedc62fd901b6
lork27 <g.lorca.lamadrid@gmail.com>
2022-01-31 15:53:39 -0400

    using requests to handle api response

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,29 @@
+from traceback import print_tb
+from markupsafe import re
+import requests
+from sys import argv
+"""
+module that takes argv 1 and uses it to request to a fake api
+"""
+
+if __name__ == "__main__":
+    if len(argv) == 2:
+        num = argv[1]
+        url = "https://jsonplaceholder.typicode.com"
+        # print("{:d} type: {}".format(request_num, type(request_num)))
+        usr = requests.get("{}/users/{}".format(url, num)).json()["name"]
+        r = requests.get("{}/todos?userId={}".format(url, num)).json()
+        # name = usr.json()["name"]
+        tasks = 0
+        total = 0
+        nametask = ""
+        for element in r:
+            if element["completed"] is True:
+                tasks += 1
+                nametask += "\t" + element["title"] + "\n"
+            total += 1
+        print("Exployee {} is done with tasks ({}/{}):".format(usr,
+                                                               tasks, total))
+        print(nametask)
+    else:
+        print("length WRONG")
