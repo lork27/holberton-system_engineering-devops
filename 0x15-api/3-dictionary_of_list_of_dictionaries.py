20ca759a8f7324f81f8df4f5185ed3347925d8c6
lork27 <g.lorca.lamadrid@gmail.com>
2022-01-31 23:28:45 -0400

    JSON overload

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+module that takes argv 1 and uses it to request to a fake api!
+"""
+import json
+import requests
+
+if __name__ == "__main__":
+    num = 2
+    url = "https://jsonplaceholder.typicode.com/"
+    users = []
+
+    for i in range(1, 11):
+        users.append(requests.get(url+'users/{}'.format(i)).json()["username"])
+    nametask = {}
+    for num, usr in enumerate(users, 1):
+        r = requests.get(url + "todos?userId={}".format(num)).json()
+        nametask[num] = []
+        for element in r:
+            tmp_dict = {'username': usr,
+                        'task': element["title"],
+                        'completed': element["completed"]
+                        }
+            nametask[num].append(tmp_dict)
+
+    with open("todo_all_employees.json", "w") as f:
+        json.dump(nametask, f)
+    print(nametask)
